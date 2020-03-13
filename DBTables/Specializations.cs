7023877f10ec38a92cd44d8a3b42239528595dfb
Samuele Lorefice <aironenerowork@gmail.com>
2020-03-13 19:17:25 +0100

    Adds Specializations table

---
+++ DBTables/Specializations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmA_Bot.DBTables {
+	public class Specializations {
+		public int Id { get; set; }
+		public string SpecializationName { get; set; }
+		public int GradeRequirementId { get; set; }
+		public int PreRequisiteSpecializationId { get; set; }
+	}
+}
