5b8b21a83b8ffd0e9c0f273d01793ae795d55223
Samuele Lorefice <aironenerowork@gmail.com>
2020-04-10 12:54:27 +0200

    Adds Id column missing on the relationships tables

--- DBTables/SpecGradePreReq.cs
+++ DBTables/SpecGradePreReq.cs
@@ -1,6 +1,7 @@
 ﻿namespace ArmABot.DBTables {
 
 	public class SpecGradePreReq {
+		public int Id { get; set; }
 		public Specialization Specialization { get; set; }
 		public Grade GradeRequired { get; set; }
 	}
--- DBTables/SpecPreReq.cs
+++ DBTables/SpecPreReq.cs
@@ -1,6 +1,7 @@
 ﻿namespace ArmABot.DBTables {
 
 	public class SpecPreReq {
+		public int Id { get; set; }
 		public Specialization Specialization { get; set; }
 		public Specialization RequisiteSpec { get; set; }
 	}
