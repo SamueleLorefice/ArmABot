c06577683b3001cba98b710105450e969a2c02ce
Samuele Lorefice <aironenerowork@gmail.com>
2020-04-14 20:45:33 +0200

    Fixes wording in program class

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using Poll = ArmABot.DBTables.Poll;
+using User = ArmABot.DBTables.User;
 
 namespace ArmABot {
 
