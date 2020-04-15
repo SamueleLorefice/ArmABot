f302ce7a27aa28f70eadffa7c7c59a2a0c1a04c4
Samuele Lorefice <aironenerowork@gmail.com>
2020-04-16 01:01:29 +0200

    Builds Database migration

---
+++ Migrations/20200415230041_TablesUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArmABot.Migrations
+{
+    public partial class TablesUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PollId",
+                table: "PollTable",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "PollTable",
+                newName: "PollId");
+        }
+    }
+}
