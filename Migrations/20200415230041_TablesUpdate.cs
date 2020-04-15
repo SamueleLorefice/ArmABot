4e9f915a7a1475a6b893b3b4f4027a0a263d849a
Samuele Lorefice <aironenerowork@gmail.com>
2020-04-16 01:03:44 +0200

    Automatic code cleanup

--- Migrations/20200415230041_TablesUpdate.cs
+++ Migrations/20200415230041_TablesUpdate.cs
@@ -1,23 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace ArmABot.Migrations
-{
-    public partial class TablesUpdate : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "PollId",
-                table: "PollTable",
-                newName: "Id");
-        }
+namespace ArmABot.Migrations {
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "Id",
-                table: "PollTable",
-                newName: "PollId");
-        }
-    }
+	public partial class TablesUpdate : Migration {
+
+		protected override void Up(MigrationBuilder migrationBuilder) {
+			migrationBuilder.RenameColumn(
+				name: "PollId",
+				table: "PollTable",
+				newName: "Id");
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder) {
+			migrationBuilder.RenameColumn(
+				name: "Id",
+				table: "PollTable",
+				newName: "PollId");
+		}
+	}
 }
